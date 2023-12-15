fc2d68462f2a15201b946da48e5acc35c8ad9327
dineshrockey <dineshofficialbsc23@gmail.com>
2023-12-15 09:57:28 -0800

    file created

---
+++ addcart/src/components/back/Data/Data.js
@@ -0,0 +1,81 @@
+const data = {
+
+    productItems:[
+
+        {
+            id: '1',
+            name: 'Beats Headphone',
+            price: '899',
+            image: "./pics/Beats - headphones.avif" ,
+
+        },
+
+        {
+            id: '2',
+            name: 'lycra Shirt',
+            price: '399',
+            image: './pics/lycras.webp',
+
+        },
+
+        {
+            id: '3',
+            name: 'Vivo y19',
+            price: '11,999',
+            image: './pics/vivo -y19.avif',
+
+        },
+
+        {
+            id: '4 ',
+            name: 'Phillips Trimmer',
+            price: '999',
+            image: './pics/phillips.jpg',
+
+        },
+
+        {
+            id: '5',
+            name: 'Realme J6',
+            price: '11,999',
+            image: './pics/realme.jpg',
+
+        },
+
+        {
+            id: '6',
+            name: 'Acer Laptop',
+            price: '19,999',
+            image: './pics/acer laptop.avif',
+
+        },
+
+        {
+            id: '7',
+            name: 'Macbook Pro',
+            price: '2999',
+            image: './pics/macbook pro.avif',
+
+        },
+
+        {
+            id: '8',
+            name: 'Redmi Pad',
+            price: '11,999',
+            image: './pics/redmi pad.jpg',
+
+        },
+
+        {
+            id: '9',
+            name: 'Puma Shoes',
+            price: '99',
+            image: './pics/puma shoes.avif',
+
+        },
+
+    ]
+}
+
+export default data;
+
---
+++ addcart/src/components/front/Signup/Signup.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Signup.css";
+
+
+const Signup = () =>{
+
+return(
+
+    <div className="signup">
+<div className="signup2">
+    
+<button id="login-1" >LOGIN</button>
+<br />
+<button id="sign-1">SIGN UP</button>
+
+</div>
+
+{/* <input placeholder="username" /> */}
+{/* <input placeholder="password"  /> */}
+
+
+    </div>    
+);
+
+
+};
+
+export default Signup;
